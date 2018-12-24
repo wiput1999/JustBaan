ed77e2914d51721cdb41ab31bb7656db64784f94
Wiput Pootong <wiput.pootong@gmail.com>
2018-12-24 11:53:40 +0700

    Add antd and styled-components

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,20 +1,7 @@
-import React, { Component } from 'react';
+import styled from "styled-components";
 
-export default class Example extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+export default styled.div`
+    background-color: #ddd;
+    width: 500px;
+    height: 500px;
+`;
