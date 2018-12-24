1fd3f521be737763e3ed18b641d61ca4d1091205
Moss <my.xmoss@gmail.com>
2018-12-24 16:17:00 +0700

    product detail

--- resources/js/components/pages/Content/index.js
+++ resources/js/components/pages/Content/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { Button } from 'antd'
-import styled from 'styled-components'
+import { Button } from "antd";
+import styled from "styled-components";
+import image from "../../../../images/content-banner.png";
+
+const Banner = styled.div`
+    height: 180;
+    width: 200
+    background-image: url(${image});
+`;
 
 class Index extends Component {
     render() {
-        return  <div></div>
+        return <Banner />;
     }
 }
 
