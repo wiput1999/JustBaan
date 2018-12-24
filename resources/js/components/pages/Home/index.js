f344323c21bc65b4c63be01eb277f50d01ff8ed6
Wiput Pootong <wiput.pootong@gmail.com>
2018-12-24 16:00:28 +0700

    Add home content

--- resources/js/components/pages/Home/index.js
+++ resources/js/components/pages/Home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import Header from "./Header";
+import Content from "./Content";
 import Footer from "./Footer";
 
 class Home extends Component {
@@ -9,6 +10,7 @@ class Home extends Component {
         return (
             <>
                 <Header />
+                <Content title="Christmas Season" />
                 <Footer />
             </>
         );
